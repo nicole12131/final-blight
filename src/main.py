b7d5c43d63ffc343b8c1af75f271d05396ac1976
DigitalRage <blaine.buckler@ucas-edu.net>
2026-04-27 09:52:35 -0600

    Co-authored-by: IsCowdell <IsCowdell@users.noreply.github.com> Co-authored-by: nicole12131 <nicole12131@users.noreply.github.com>

---
+++ src/main.py
@@ -0,0 +1,31 @@
+import pygame 
+import os 
+
+class skip_button: 
+    skip = #Variable 
+    def __init__(self,text,x,y,width, height, color ):
+        self.rect = pygame.rect(text)
+        self.rect = pygame.rect(x)
+        self.rect = pygame.rect(y)
+        self.rect = pygame.rect(width)
+        self.rect = pygame.rect(height)
+        self.rect = pygame.rect(color)
+
+    def draw(self,screen): 
+        #more thingy 
+        #more thingy 
+        #more thingy 
+
+    
+     def clicked(self,event):
+        if event == #pygame.#MOUSEBUTTONDWOW: 
+            if self #collidepoint(event.pos)
+                #return it true 
+        
+        return #false 
+
+
+
+while skip_button == False: 
+   
+   
